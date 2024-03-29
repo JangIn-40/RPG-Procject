447b5d531c528c2fdb522c249107296d8c4b0f0d
JangIn-40 <156999923+JangIn-40@users.noreply.github.com>
2024-03-29 21:04:29 +0900

    Raycasting추가
    Ray에 대해서 배움, Ray란 시작지점에서 도착지점까지 보이지 않는 raser를 쏘는거라 생각해도 될듯하다. 이 ray로 플레이어 움직임이나 총알을 발사한다거나 아니면 플레이어가 공격을 할때 적의 콜라이더가 여러개일 경우 공격이 여러번 적용될텐데 ray를 쏴서 타격을 한번만 맞추도록 해줄수 있을것 같다.

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -7,14 +7,25 @@ public class Mover : MonoBehaviour
 {
     [SerializeField] Transform target;
 
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        GetComponent<NavMeshAgent>().destination = target.position;
+        if (Input.GetMouseButtonDown(0))
+        {
+            MoveToCursor();
+        }
+
+    }
+
+    void MoveToCursor()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit Hit;
+        bool hasHit = Physics.Raycast(ray, out Hit);
+        if(hasHit)
+        {
+            GetComponent<NavMeshAgent>().destination = Hit.point;
+        }
     }
 }

9058d60b80174ac060b675ca593916bb8f4725ee
JangIn-40 <156999923+JangIn-40@users.noreply.github.com>
2024-04-02 10:04:08 +0900

    죽음 애니메이션 추가
    애니메이션에서 any state로 죽음 애니메이션을 추가함

--- Assets/Scripts/Combat/Health.cs
+++ Assets/Scripts/Combat/Health.cs
@@ -5,10 +5,30 @@ namespace RPG.Combat{
     {
         [SerializeField] float health = 100f;
 
+        Animator animator;
+
+        bool isDie = false;
+
+        void Start()
+        {
+            animator = GetComponent<Animator>();
+        }
+
         public void TakeDamage(float damage)
         {
             health = Mathf.Max(health - damage, 0);
-            print(health);
-        } 
+            if(health == 0)
+            {
+                Die();
+            }
+
+        }
+
+        private void Die()
+        {
+            if(isDie) { return; }
+            isDie = true;
+            animator.SetTrigger("die");
+        }
     }
 }

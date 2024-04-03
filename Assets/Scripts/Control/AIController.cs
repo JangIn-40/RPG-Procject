d84e2cabe0e7214611c3c4575808da971b34bd9e
JangIn-40 <156999923+JangIn-40@users.noreply.github.com>
2024-04-03 14:09:57 +0900

    AI 가딩 구현
    AI가 플레이어를 쫓아가고 나서 놓쳤을 경우 가만히 서있는것이 아닌 어느정도 서있다가 자기 위치로 되돌아가게 구현했다. 초기위치를 변수에 저장했고 float변수와 Time.deltatime으로 가만히 서있는시간을 설정해주었다.

--- Assets/Scripts/Control/AIController.cs
+++ Assets/Scripts/Control/AIController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using RPG.Combat;
 using RPG.Core;
+using RPG.Movement;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -10,17 +11,25 @@ namespace RPG.Control
     public class AIController : MonoBehaviour
     {
         [SerializeField] float chaseDistance = 5f;
+        [SerializeField] float suspicionTime = 2f;
 
         Fighter fighter;
         Health health;
+        Mover mover;
+        ActionScheduler actionScheduler;
         GameObject player;
+
+        Vector3 guardPosition;
+        float timeSinceLastSawPlayer = Mathf.Infinity;
 
         void Start()
         {
-
             fighter = GetComponent<Fighter>();
-            player = GameObject.FindWithTag("Player");
+            mover = GetComponent<Mover>();
             health = GetComponent<Health>();
+            actionScheduler = GetComponent<ActionScheduler>();
+            player = GameObject.FindWithTag("Player");
+            guardPosition = transform.position;
         }
 
         void Update()
@@ -28,12 +37,20 @@ namespace RPG.Control
             if(health.IsDie()) { return; }
             if(InAttackRangeOfPlayer() && fighter.CanAttack(player))
             {
+                timeSinceLastSawPlayer = 0;
                 fighter.Attack(player);
+            }
+            //의심상태 suspicion
+            else if(timeSinceLastSawPlayer < suspicionTime)
+            {
+                actionScheduler.CancelCurrentAction();
             }
             else
             {
-                fighter.Cancel();
+                mover.StartMoveToAction(guardPosition);
             }
+
+            timeSinceLastSawPlayer += Time.deltaTime;
         }
 
         private bool InAttackRangeOfPlayer()
@@ -43,7 +60,7 @@ namespace RPG.Control
         }
 
         //Called by Untiy
-        void OnDrawGizmos() 
+        void OnDrawGizmosSelected() 
         {
             Gizmos.color = Color.red;
             Gizmos.DrawWireSphere(transform.position, chaseDistance);

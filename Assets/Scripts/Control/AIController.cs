37ff6d7c24ff9c19d29fc0da4490b31714c9b09d
JangIn-40 <156999923+JangIn-40@users.noreply.github.com>
2024-04-04 20:13:04 +0900

    적 정찰 행동
    patrolpath안의 있던 private함수들을 public으로 바꾸어서 AIController에서 사용했다. 했던 작업들은 전에서 했던 player이동구현과 비슷하다.  그리고 적들이 웨이포인트를 이동만 하지 않고 웨이포인트에서 체류할수있게 해주었다. 그리고 쿨타임과 같은 시간개념을 해주긴 위해선 게임시작시 Mathf.Infinity를 사용해서 무한으로 늘려줘야한다. 아니면 능력활성화시에 설정해주거나

--- Assets/Scripts/Control/AIController.cs
+++ Assets/Scripts/Control/AIController.cs
@@ -4,7 +4,8 @@ using RPG.Combat;
 using RPG.Core;
 using RPG.Movement;
 using UnityEngine;
-using UnityEngine.UIElements;
+using RPG.Control;
+using System;
 
 namespace RPG.Control
 {
@@ -12,6 +13,9 @@ namespace RPG.Control
     {
         [SerializeField] float chaseDistance = 5f;
         [SerializeField] float suspicionTime = 2f;
+        [SerializeField] PatrolPath patrolPath;
+        [SerializeField] float waypointTolerance = 1f;
+        [SerializeField] float waypintDwellTime = 2f;
 
         Fighter fighter;
         Health health;
@@ -21,6 +25,8 @@ namespace RPG.Control
 
         Vector3 guardPosition;
         float timeSinceLastSawPlayer = Mathf.Infinity;
+        float timeSinceArrivedWaypoint = Mathf.Infinity;
+        int currentWaypointIndex = 0;
 
         void Start()
         {
@@ -38,19 +44,66 @@ namespace RPG.Control
             if(InAttackRangeOfPlayer() && fighter.CanAttack(player))
             {
                 timeSinceLastSawPlayer = 0;
-                fighter.Attack(player);
+                AttackBehaviour();
             }
             //의심상태 suspicion
             else if(timeSinceLastSawPlayer < suspicionTime)
             {
-                actionScheduler.CancelCurrentAction();
+                SuspicionBehaviour();
             }
             else
             {
-                mover.StartMoveToAction(guardPosition);
+                PatrolBehaviour();
             }
 
             timeSinceLastSawPlayer += Time.deltaTime;
+            timeSinceArrivedWaypoint += Time.deltaTime;
+        }
+
+        private void AttackBehaviour()
+        {
+            fighter.Attack(player);
+        }
+
+        private void SuspicionBehaviour()
+        {
+            actionScheduler.CancelCurrentAction();
+        }
+
+        private void PatrolBehaviour()
+        {
+            Vector3 nextPosition = guardPosition;
+
+            if(patrolPath != null)
+            {
+                if(AtWaypoint())
+                {
+                    timeSinceArrivedWaypoint = 0;
+                    CycleWaypoint();
+                }
+                nextPosition = GetCurrentWaypoint();
+            }
+            if(timeSinceArrivedWaypoint > waypintDwellTime)
+            {
+                mover.StartMoveToAction(nextPosition);
+            }
+        }
+
+        private bool AtWaypoint()
+        {
+            float distanceToWaypoint = Vector3.Distance(transform.position, GetCurrentWaypoint());
+            return distanceToWaypoint < waypointTolerance;
+        }
+
+        private void CycleWaypoint()
+        {
+
+            currentWaypointIndex = patrolPath.GetNextIndex(currentWaypointIndex);
+        }
+
+        private Vector3 GetCurrentWaypoint()
+        {
+            return patrolPath.GetWaypoint(currentWaypointIndex);
         }
 
         private bool InAttackRangeOfPlayer()

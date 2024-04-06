28165417940fcf358627a77b12511edbf446a33d
JangIn-40 <156999923+JangIn-40@users.noreply.github.com>
2024-04-06 17:20:56 +0900

    씬 로딩 포탈
    SceneManager.LoadScene()으로 로딩 씬으로 갈수있는 포탈을 만듬. 만든 방식은 cinematic만든것과 동일하게 trigger방식임

---
+++ Assets/Scripts/SceneManagement/Portal.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace RPG.SceneManagement
+{
+    public class Portal : MonoBehaviour 
+    {
+        [SerializeField] int sceneToLoad = -1;
+
+        void OnTriggerEnter(Collider other) 
+        {
+            if(other.gameObject.CompareTag("Player"))
+            {
+                SceneManager.LoadScene(sceneToLoad);
+            }    
+        }
+    }
+}

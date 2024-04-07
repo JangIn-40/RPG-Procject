b2eae915809b690cb8f68d372162472d879e44af
JangIn-40 <156999923+JangIn-40@users.noreply.github.com>
2024-04-07 14:34:32 +0900

    enum을 활용해서 Portal짝 맞춤
    enum을 활용해서 각각의 포탈출구와 입구를 짝을 맞추어주었다. 그리고 포탈에서 나올때 플레이어의 위치와 회전값이 이상하지않도록 설정해주었다.
    새로배운함수 : Navmeshagent.Warp()

--- Assets/Scripts/SceneManagement/Portal.cs
+++ Assets/Scripts/SceneManagement/Portal.cs
@@ -1,12 +1,21 @@
+using System;
 using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.AI;
 
 namespace RPG.SceneManagement
 {
     public class Portal : MonoBehaviour 
     {
+        enum DestinationIdentifier
+        {
+            A, B, C, D, E
+        }
+
         [SerializeField] int sceneToLoad = -1;
+        [SerializeField] Transform spawnPoint;
+        [SerializeField] DestinationIdentifier destination;
 
 		
         void OnTriggerEnter(Collider other) 
@@ -21,8 +30,27 @@ namespace RPG.SceneManagement
         {
 			DontDestroyOnLoad(this.gameObject);
 			yield return SceneManager.LoadSceneAsync(sceneToLoad);
-			print("Scene Loaded");
+			Portal otherPortal = GetOtherPortal();
+            UpdatePlayer(otherPortal);
 			Destroy(this.gameObject);
+        }
+
+        private Portal GetOtherPortal()
+        {
+            foreach(Portal portal in FindObjectsOfType<Portal>())
+            {
+                if(portal == this) continue;
+                if(portal.destination != destination) continue;
+                return portal;
+            }
+            return null;
+        }
+
+        private void UpdatePlayer(Portal otherPortal)
+        {
+            GameObject player = GameObject.FindWithTag("Player");
+            player.GetComponent<NavMeshAgent>().Warp(otherPortal.spawnPoint.position);
+            player.transform.rotation = otherPortal.spawnPoint.rotation;
         }
     }
 }

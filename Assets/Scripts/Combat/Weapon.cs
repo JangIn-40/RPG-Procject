5b0a649592b43b01d7a3dbdb8eafd108ba0c778b
JangIn-40 <156999923+JangIn-40@users.noreply.github.com>
2024-04-10 15:11:24 +0900

    새로운 무기 습득 시 기존 무기 파괴, 화살Trail추가
    Trail로 화살이 날아갈때 날아가는흔적이 잘보이도록 설정해주었고 transform.Find로 새로운걸 습득하기전에 기존에 있던 무기를 파괴하도록 로직을 짜주었다.
    새로운 기능 : Trail
    새로운 함수 : transform.Find()

--- Assets/Scripts/Combat/Weapon.cs
+++ Assets/Scripts/Combat/Weapon.cs
@@ -1,3 +1,4 @@
+using System;
 using RPG.Core;
 using UnityEngine;
 
@@ -13,18 +14,36 @@ namespace RPG.Combat
         [SerializeField] bool isRightHanded = true;
         [SerializeField] Projectile projectile = null;
 
+        const string weaponName = "Weapon";
+
         public void Spwan(Transform rightHand, Transform leftHand, Animator animator)
         {
+            DestroyOldWeapon(rightHand, leftHand);
+
             if(equippedPrefab != null)
             {
                 Transform handTransform = GetTransform(rightHand, leftHand);
-                Instantiate(equippedPrefab, handTransform);
+                GameObject weapon = Instantiate(equippedPrefab, handTransform);
+                weapon.name = weaponName;
             }
             if (animatorOverride != null)
             {
                 animator.runtimeAnimatorController = animatorOverride;
             }
 
+        }
+
+        private void DestroyOldWeapon(Transform rightHand, Transform leftHand)
+        {
+            Transform oldWeapon = rightHand.Find(weaponName);
+            if (oldWeapon == null)
+            {
+                oldWeapon = leftHand.Find(weaponName);
+            }
+            if(oldWeapon == null) return;
+
+			oldWeapon.name = "DESTROYING";
+			Destroy(oldWeapon.gameObject);
         }
 
         private Transform GetTransform(Transform rightHand, Transform leftHand)

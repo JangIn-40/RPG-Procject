6ed3d551bcfabb5b1d6295c13115caa6c3071cf3
JangIn-40 <156999923+JangIn-40@users.noreply.github.com>
2024-04-12 11:57:52 +0900

    코루틴은 게임오브젝트 비활성화 시 중지!
    코루틴은 게임오브젝트를 비활성화시 중지되므로 코르틴에서 게임오브젝트를 비활성화, 활성화해주면 안됀다. 컴포넌트를 해주자. 그리고 콜라이더는 정확한 이름이 아니고 그냥 콜라이더라 써줘도 비활성화 된다. 했던 기능은 픽업 아이템을 시간이 지나면 다시 스폰하게 해주었다.

--- Assets/Scripts/Combat/WeaponPickup.cs
+++ Assets/Scripts/Combat/WeaponPickup.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,15 +8,34 @@ namespace RPG.Combat
     public class WeaponPickup : MonoBehaviour
     {
         [SerializeField] Weapon weapon = null;
+        [SerializeField] float respawnTime = 5f;
 
         void OnTriggerEnter(Collider other) 
         {
             if(other.gameObject.CompareTag("Player"))
             {
                 other.GetComponent<Fighter>().EquipWeapon(weapon);
-                Destroy(gameObject);
+                StartCoroutine(HideForSeconds(respawnTime));
             }
         }
+
+        IEnumerator HideForSeconds(float seconds)
+        {
+            ShowPickup(false);
+            yield return new WaitForSeconds(seconds);
+            ShowPickup(true);
+        }
+
+        void ShowPickup(bool shouldShow)
+        {
+            GetComponent<SphereCollider>().enabled = shouldShow;
+            foreach (Transform child in transform)
+            {
+                child.gameObject.SetActive(shouldShow);
+            }
+        }
+
+
     }
 }
 
